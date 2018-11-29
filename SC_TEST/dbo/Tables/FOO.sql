dc99b5c586c427e50649c910bda9ad3618c30be9
Geoff Irwin <geoff.irwin@Marine.ie>
2018-11-29 16:48:21 +0000

    inital remote commit

--- SC_TEST/dbo/Tables/FOO.sql
+++ SC_TEST/dbo/Tables/FOO.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[FOO] (
     [fooKey]   INT          NOT NULL,
     [fooValue] VARCHAR (25) NULL,
+    [anotherValue] NVARCHAR(50) NULL, 
     PRIMARY KEY CLUSTERED ([fooKey] ASC) WITH (FILLFACTOR = 90)
 );
 
